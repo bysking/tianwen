0780e458661b2c28c5aac6454cb77bd4edc5e1d3
bysking <bysking@qq.com>
2023-04-22 14:13:24 +0800

    feat: 添加数据库链接信息

--- tw-server/db/index.js
+++ tw-server/db/index.js
@@ -1,2 +1,8 @@
 const mongoose = require("mongoose");
 const db = mongoose.connect("mongodb://localhost/bysking_database");
+
+db.then(() => {
+  console.error("****数据库连接成功****");
+}).catch(() => {
+  console.error("****数据库连接失败****");
+});
