376ca3ea526ff59707a80b8cf70657af1d6f871f
chenpengxu <pengxu.chen@viatris.com>
2024-01-27 15:52:36 +0800

    feat: add eslint stylelint prettier

--- apps/react-project/src/index.tsx
+++ apps/react-project/src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 // const root = document.getElementById('root');
-const root = document.querySelector("#root");
-console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("BASE_ENV", process.env.BASE_ENV);
+const root = document.querySelector('#root');
+// console.log('NODE_ENV', process.env.NODE_ENV);
+// console.log('BASE_ENV', process.env.BASE_ENV);
 
 if (root) {
   createRoot(root).render(<App />);
