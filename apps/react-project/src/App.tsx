98b369fe83eb71f939d9c50e689632c283c563ad
chenpengxu <pengxu.chen@viatris.com>
2024-01-27 16:42:40 +0800

    feat: add eslint

--- apps/react-project/src/App.tsx
+++ apps/react-project/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 // import "./App.css";
 import lessStyles from '@/App.less';
 import imageWord from '@/assets/map.jpg';
 
 function App() {
+  const logFn = (count: number) => {
+    /** 34 */
+
+    return count + 1;
+  };
+  logFn(2);
   return (
     <h2>
       222 Hello East_White
