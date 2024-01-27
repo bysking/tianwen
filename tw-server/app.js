1eda6427b17e44e1cb6eb44702baef65a491c79d
chenpengxu <pengxu.chen@viatris.com>
2024-01-27 11:49:23 +0800

    feat: add cfg

--- tw-server/app.js
+++ tw-server/app.js
@@ -1,26 +1,46 @@
+const session = require("koa-session");
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const cors = require("koa2-cors");
 require("./db");
 
 const app = new Koa();
-app.use(bodyParser()); // 解析request的body
-
 // 处理跨域
 app.use(
   cors({
-    // exposeHeaders: ["WWW-Authenticate", "Server-Authorization", "Date"],
-    // maxAge: 100,
+    origin: "*",
+    exposeHeaders: [
+      "WWW-Authenticate",
+      "Server-Authorization",
+      "Date",
+      "bysking",
+    ],
+    maxAge: 100,
     credentials: true,
-    // allowMethods: ["GET", "POST", "OPTIONS"],
-    // allowHeaders: [
-    //   "Content-Type",
-    //   "Authorization",
-    //   "Accept",
-    //   "X-Custom-Header",
-    //   "anonymous",
-    // ],
+    // rolling: true,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Accept",
+      "X-Custom-Header",
+      "bysking",
+    ],
   })
+);
+app.keys = ["test"];
+app.use(bodyParser()); // 解析request的body
+app.use(
+  session(
+    {
+      key: "koa:sess", // 默认的cookie名称
+      maxAge: 100, // session过期时间，单位：毫秒
+      overwrite: true, // 是否覆盖已有的同名cookie，默认为true
+      httpOnly: false, // 是否只能通过http访问cookie，默认为true
+      signed: true, // 是否对cookie进行签名，默认为true
+    },
+    app
+  )
 );
 
 const router = require("./router");
--- tw-server/router/module-a/index.js
+++ tw-server/router/module-a/index.js
@@ -1,9 +1,21 @@
 module.exports = (router) => {
   router.get("/test", async (ctx, next) => {
     const { username, password } = ctx.request.body;
-    console.log(ctx.request.body);
 
-    await next();
+    const user = ctx.session.user;
+
+    if (user) {
+      console.log("session", user);
+    } else {
+      ctx.session.user = Math.random();
+    }
+
+    let bb = ctx.cookies.get("bysking");
+    if (bb) {
+      console.log("cookie", bb);
+    } else {
+      ctx.cookies.set("bysking", Math.random());
+    }
 
     const result = {
       code: 200,
@@ -13,6 +25,7 @@ module.exports = (router) => {
       msg: "ok",
     };
     ctx.response.body = result;
-    return;
+
+    await next();
   });
 };
