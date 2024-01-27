008d620231d7b9caf7c19d766606a63e929db680
chenpengxu <pengxu.chen@viatris.com>
2024-01-27 14:34:17 +0800

    feat: add img support

--- apps/react-project/build/webpack.base.ts
+++ apps/react-project/build/webpack.base.ts
@@ -34,6 +34,7 @@ const baseConfig: Configuration = {
     path: path.join(__dirname, "../dist"), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
     publicPath: "/", // 打包后文件的公共前缀路径
+    assetModuleFilename: "images/[hash][ext][query]", // 这里自定义输出文件名的方式是，将某些资源发送到指定目录
   },
   // loader 配置
   module: {
@@ -67,6 +68,18 @@ const baseConfig: Configuration = {
       {
         test: sassRegex,
         use: [...styleLoadersArray, "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // 匹配图片文件
+        type: "asset", // type选择asset
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 小于10kb转base64
+          },
+        },
+        generator: {
+          filename: "static/images/[hash][ext][query]", // 文件输出目录和命名
+        },
       },
     ],
   },
--- apps/react-project/src/App.tsx
+++ apps/react-project/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 // import "./App.css";
 import lessStyles from "@/App.less";
+import imageWord from "@/assets/map.jpg";
 
 function App() {
   return (
     <h2>
       Hello East_White
       <div className={lessStyles["bysking"]}>34</div>
+      <img width={300} src={imageWord} alt="imageWord" />
     </h2>
   );
 }
