a20cbbddf8de2ede34c8e4ceea7442fe396f0487
chenpengxu <pengxu.chen@viatris.com>
2024-01-27 12:12:25 +0800

    feat: add base loader

--- apps/react-project/build/webpack.base.ts
+++ apps/react-project/build/webpack.base.ts
@@ -1,4 +1,6 @@
 import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
 const path = require("path");
 
 const baseConfig: Configuration = {
@@ -12,13 +14,34 @@ const baseConfig: Configuration = {
   },
   // loader 配置
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /.css$/, //匹配 css 文件
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
-  // plugins 的配置
-  plugins: [],
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
+      template: path.join(__dirname, "../public/index.html"),
+      // 压缩html资源
+      minify: {
+        collapseWhitespace: true, //去空格
+        removeComments: true, // 去注释
+      },
+    }),
+  ],
 };
 
-module.exports = baseConfig;
+export default baseConfig;
