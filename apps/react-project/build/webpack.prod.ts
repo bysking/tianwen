2d56560cb224a82c4917f046646c23d4f925c6f7
chenpengxu <pengxu.chen@viatris.com>
2024-01-27 15:22:34 +0800

    feat: common js exetract

--- apps/react-project/build/webpack.prod.ts
+++ apps/react-project/build/webpack.prod.ts
@@ -4,6 +4,9 @@ import baseConfig from "./webpack.base";
 import CopyPlugin from "copy-webpack-plugin";
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import TerserPlugin from "terser-webpack-plugin";
+import CompressionPlugin from "compression-webpack-plugin";
 
 const prodConfig: Configuration = merge(baseConfig, {
   mode: "production", // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
@@ -20,7 +23,54 @@ const prodConfig: Configuration = merge(baseConfig, {
     new MiniCssExtractPlugin({
       filename: "static/css/[name]-[hash:8].css", // 抽离css的输出目录和名称
     }),
+    new CompressionPlugin({
+      test: /\.(js|css)$/, // 只生成css,js压缩文件
+      filename: "[path][base].gz", // 文件命名
+      algorithm: "gzip", // 压缩格式,默认是gzip
+      threshold: 10240, // 只有大小大于该值的资源会被处理。默认值是 10k
+      minRatio: 0.8, // 压缩率,默认值是 0.8
+    }),
   ],
+  optimization: {
+    runtimeChunk: {
+      name: "mainifels",
+    },
+    minimizer: [
+      new CssMinimizerPlugin(), // 压缩css
+      new TerserPlugin({
+        parallel: true, // 开启多线程压缩
+        terserOptions: {
+          compress: {
+            pure_funcs: ["console.log"], // 删除console.log
+          },
+        },
+      }),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /node_modules/, // 只匹配node_modules里面的模块
+          name: "vendors", // 提取文件命名为vendors,js后缀和chunkhash会自动加
+          minChunks: 1, // 只要使用一次就提取出来
+          chunks: "initial", // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0, // 提取代码体积大于0就提取出来
+          priority: 1, // 提取优先级为1
+        },
+        commons: {
+          // 提取页面公共代码
+          name: "commons", // 提取文件命名为commons
+          minChunks: 2, // 只要使用两次就提取出来
+          chunks: "initial", // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0, // 提取代码体积大于0就提取出来
+        },
+      },
+    },
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 4000000, // 整数类型（以字节为单位）
+    maxEntrypointSize: 5000000, // 整数类型（以字节为单位）
+  },
 });
 
 export default prodConfig;
