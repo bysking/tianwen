10f95d84fa54be902146f445ac12e91621e5ef4b
bysking <pengxu.chen@viatris.com>
2024-04-18 10:09:03 +0800

    feat: 添加父子应用的通信

--- tw-frontend/src/pages/Welcome.tsx
+++ tw-frontend/src/pages/Welcome.tsx
@@ -1,5 +1,9 @@
+import { useModel } from '@umijs/max';
+
 const Welcome = () => {
-  return <div>欢迎</div>;
+  // 主应用获取暴露给子应用的数据
+  const { masterState, setMasterState } = useModel('@@qiankunStateForSlave');
+  return <div>欢迎{masterState}</div>;
 };
 
 export default Welcome;
